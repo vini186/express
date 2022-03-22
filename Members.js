611ce166117bea257bffd9a24a0aac76831cd959
vini186 <kallavineeta@gmail.com>
2022-03-22 12:17:52 +0530

    commit

---
+++ Members.js
@@ -0,0 +1,23 @@
+const members = [
+    {
+        id:1,
+        name:'joh deo',
+        email:'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name:'marydeo',
+        email:'mary@gmail.com',
+        status: 'active'
+    },
+    {
+        id:3,
+        name:'smith',
+        email:'smith@gmail.com',
+        status: 'active'
+    }
+
+];
+
+module.exports = members;
